a3de3a0bf88d04d2ab1d3b2d7fef7f1135e3cda9
smltrs0 <49120338+smltrs0@users.noreply.github.com>
2019-09-23 10:28:36 -0400

    carrito de compra
    aun trabajando en el carrito

--- controller/get_libro_id.php
+++ controller/get_libro_id.php
@@ -11,28 +11,25 @@ session_start();
     require(TOOLS.'get.php');
 
 // como la consulta la vamos a hacer usando ajax... 
+// de esta manera verificamos la existencia del producto
    // $id_request = $_POST['product_code'];
+   // $cantidad = $_POST['cantidad'];
 
-    $libro = get::libro_by_id(4);
+    $libro = get::libro_by_id(1);
 echo "<pre>";
 print_r($libro);
 echo "<pre>";
 
 
 
-
 if(isset($libro) && !empty($libro))
 {
-
 	if(isset($_SESSION["carrito"]))
 	{
-
 		$item_array_id = array_column($_SESSION["carrito"], "item_id");
 		// con in_array()lo que hacemos es comprobar si un valor existe en el array
 		if(!in_array($libro["id_libro"], $item_array_id))
-
 		{
-			$count = count($_SESSION["carrito"]);
 			$item_array = array(
 			'item_id' => $libro["id_libro"],
 			'item_name' => $libro["titulo"],
@@ -43,32 +40,50 @@ if(isset($libro) && !empty($libro))
 
 		}else
 			{
-				// Aqui se tendria que actualizar la cantidad
+				// Aqui se tendria que actualizar la cantidad 
 				echo '<script>alert("El producto ya se encuentra agregado");
 				</script>';
 			}
-
 	}
 
-	else{
+	else
+		{
 			$item_array = array(
 			'item_id' => $libro["id_libro"],
 			'item_name' => $libro["titulo"],
 			'item_price' => $libro["precio"]
 			);
-
 			$_SESSION["carrito"][md5($libro["id_libro"])] = $item_array;
 		}
-
 }
 else 
-{
-	echo '<script>alert("El producto No existe");
+	{
+		echo '<script>alert("El producto No existe");
 				</script>';
-}
-
+	}
+echo "***************Esto es lo que esta almacenado en la sesion*********************</br>";
 print_r($_SESSION["carrito"]);
 //session_destroy();
 
+//Eliminamos el id seleccionado del array
+//unset($_SESSION["carrito"][md5($libro["id_libro"])]); 
 
+
+
+echo "***************Esto es lo que imprimiria los productos agregados al carrito *********************</br>";
+
+$sum=0;
+foreach($_SESSION["carrito"] as $keys => $values){
+
+echo "ID: ".$values["item_id"]."<br>";
+echo "Precio: ".$values["item_price"]."<br>"; 
+$sum += $values["item_price"];
+
+}
+
+
+echo "Subtotal: ".$sum."<br>";
+echo "IVA: ".($iva=(12 / 100) * $sum)."<br>";
+$total=($iva+$sum);
+echo "Neto a pagar: ".$total;
 ?>
