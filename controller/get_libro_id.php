4adfaf74d3014b0bf3e420dc2ce3fdb8e4a904ca
smltrs0 <49120338+smltrs0@users.noreply.github.com>
2019-09-22 23:48:16 -0400

    el controlador del carrito casi terminado

--- controller/get_libro_id.php
+++ controller/get_libro_id.php
@@ -11,7 +11,7 @@ session_start();
     require(TOOLS.'get.php');
 
 // como la consulta la vamos a hacer usando ajax... 
-    $id_request = $_POST['product_code'];
+   // $id_request = $_POST['product_code'];
 
     $libro = get::libro_by_id(4);
 echo "<pre>";
@@ -21,51 +21,54 @@ echo "<pre>";
 
 
 
-if(isset($libro))
+if(isset($libro) && !empty($libro))
 {
 
 	if(isset($_SESSION["carrito"]))
 	{
 
-	$item_array_id = array_column($_SESSION["carrito"], "item_id");
+		$item_array_id = array_column($_SESSION["carrito"], "item_id");
+		// con in_array()lo que hacemos es comprobar si un valor existe en el array
+		if(!in_array($libro["id_libro"], $item_array_id))
 
-	if(!in_array($_GET["id_prod"], $item_array_id))
-
-	{
-		$count = count($_SESSION["carrito"]);
-		$item_array = array(
-		'item_id' => $libro["id_libro"],
-		'item_name' => $libro["titulo"],
-		'item_price' => $libro["precio"]
-		);
-
-		$_SESSION["carrito"][$count] = $item_array;
-
-	}else
 		{
-		echo '<script>alert("El producto ya se encuentra agregado")</script>';
-		}
+			$count = count($_SESSION["carrito"]);
+			$item_array = array(
+			'item_id' => $libro["id_libro"],
+			'item_name' => $libro["titulo"],
+			'item_price' => $libro["precio"]
+			);
+
+			$_SESSION["carrito"][md5($libro["id_libro"])] = $item_array;
+
+		}else
+			{
+				// Aqui se tendria que actualizar la cantidad
+				echo '<script>alert("El producto ya se encuentra agregado");
+				</script>';
+			}
 
 	}
 
 	else{
+			$item_array = array(
+			'item_id' => $libro["id_libro"],
+			'item_name' => $libro["titulo"],
+			'item_price' => $libro["precio"]
+			);
 
-	$item_array = array(
-
-	'item_id' => $libro["id_libro"],
-
-	'item_name' => $libro["titulo"],
-
-	'item_price' => $libro["precio"]
-
-	);
-
-	$_SESSION["carrito"][0] = $item_array;
-
+			$_SESSION["carrito"][md5($libro["id_libro"])] = $item_array;
 		}
 
 }
+else 
+{
+	echo '<script>alert("El producto No existe");
+				</script>';
+}
 
 print_r($_SESSION["carrito"]);
+//session_destroy();
+
 
 ?>
