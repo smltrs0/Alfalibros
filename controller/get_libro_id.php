68c98c217ff16fd575ad12f4b9f44d52292ae03c
smltrs0 <49120338+smltrs0@users.noreply.github.com>
2019-09-22 20:17:49 -0400

    actualizado...

--- controller/get_libro_id.php
+++ controller/get_libro_id.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 
      // CARGANDO LAS CONSTANTES DE RUTAS
     require('../config.path.php');
@@ -9,85 +10,62 @@
     require(TOOLS.'cleaning.php');
     require(TOOLS.'get.php');
 
+// como la consulta la vamos a hacer usando ajax... 
     $id_request = $_POST['product_code'];
 
-    $libro = get::libro_by_id(1);
+    $libro = get::libro_by_id(4);
 echo "<pre>";
 print_r($libro);
 echo "<pre>";
 
-if(isset($_POST["product_code"]))
-{
-	foreach($_POST as $key => $value){
-		$new_product[$key] = filter_var($value, FILTER_SANITIZE_STRING); // Creamos el array del nuevo producto
-	}
-	
-	//we need to get product name and price from database.
-	$statement = $mysqli_conn->prepare("SELECT product_name, product_price FROM products_list WHERE product_code=? LIMIT 1");
-	$statement->bind_param('s', $new_product['product_code']);
-	$statement->execute();
-	$statement->bind_result($product_name, $product_price);
-	
 
-	while($statement->fetch()){ 
-		$new_product["product_name"] = $product_name; //fetch product name from database
-		$new_product["product_price"] = $product_price;  //fetch product price from database
-		
-		if(isset($_SESSION["products"])){  //if session var already exist
-			if(isset($_SESSION["products"][$new_product['product_code']])) //check item exist in products array
-			{
-				unset($_SESSION["products"][$new_product['product_code']]); //unset old item
-			}			
-		}
-		
-		$_SESSION["products"][$new_product['product_code']] = $new_product;	//update products with new item array	
-	}
-	
- 	$total_items = count($_SESSION["products"]); //contamos todos los items
-	die(json_encode(array('items'=>$total_items))); //output json 
 
-}
 
-################## lista de los productos en el carrito ###################
-if(isset($_POST["load_cart"]) && $_POST["load_cart"]==1)
+if(isset($libro))
 {
 
-	if(isset($_SESSION["products"]) && count($_SESSION["products"])>0){ //if we have session variable
-		$cart_box = '<ul class="cart-products-loaded">';
-		$total = 0;
-		foreach($_SESSION["products"] as $product){ //loop though items and prepare html content
-			
-			//set variables to use them in HTML content below
-			$product_name = $product["product_name"]; 
-			$product_price = $product["product_price"];
-			$product_code = $product["product_code"];
-			$product_cantidad = $product["product_cantidad"];
-			
-			$cart_box .=  "<li> $product_name (Cantidad : $product_cantidad  |  ) &mdash; $currency ".sprintf("%01.2f", ($product_price * $product_cantidad)). " <a href=\"#\" class=\"remove-item\" data-code=\"$product_code\">&times;</a></li>";
-			$subtotal = ($product_price * $product_cantidad);
-			$total = ($total + $subtotal);
-		}
-		$cart_box .= "</ul>";
-		$cart_box .= '<div class="cart-products-total">Total : '.$currency.sprintf("%01.2f",$total).' <u><a href="pagar_carrito.php" title="Ver los itens y Procesar la compra">Terminar Compra</a></u></div>';
-		die($cart_box); //exit and output content
-	}else{
-		die("Tu carrito esta vacio"); //we have empty cart
-	}
-}
-
-################# Eliminar items del carrito de compra ################
-/*
-if(isset($_GET["remove_code"]) && isset($_SESSION["products"]))
-{
-	$product_code   = filter_var($_GET["remove_code"], FILTER_SANITIZE_STRING); //get the product code to remove
-
-	if(isset($_SESSION["products"][$product_code]))
+	if(isset($_SESSION["carrito"]))
 	{
-		unset($_SESSION["products"][$product_code]);
+
+	$item_array_id = array_column($_SESSION["carrito"], "item_id");
+
+	if(!in_array($_GET["id_prod"], $item_array_id))
+
+	{
+		$count = count($_SESSION["carrito"]);
+		$item_array = array(
+		'item_id' => $libro["id_libro"],
+		'item_name' => $libro["titulo"],
+		'item_price' => $libro["precio"]
+		);
+
+		$_SESSION["carrito"][$count] = $item_array;
+
+	}else
+		{
+		echo '<script>alert("El producto ya se encuentra agregado")</script>';
+		}
+
 	}
-	
- 	$total_items = count($_SESSION["products"]);
-	die(json_encode(array('items'=>$total_items)));
+
+	else{
+
+	$item_array = array(
+
+	'item_id' => $libro["id_libro"],
+
+	'item_name' => $libro["titulo"],
+
+	'item_price' => $libro["precio"]
+
+	);
+
+	$_SESSION["carrito"][0] = $item_array;
+
+		}
+
 }
-*/
- ?>
+
+print_r($_SESSION["carrito"]);
+
+?>
